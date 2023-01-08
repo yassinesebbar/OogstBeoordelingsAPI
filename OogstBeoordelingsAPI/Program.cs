0080e6648b482e06f939d5773c169510e7f8b3f8
Yassine Sebbar <yassine.sebbar@hotmail.com>
2023-01-08 22:03:07 +0100

    Use swagger in online env

--- OogstBeoordelingsAPI/Program.cs
+++ OogstBeoordelingsAPI/Program.cs
@@ -100,7 +100,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAuthentication();
